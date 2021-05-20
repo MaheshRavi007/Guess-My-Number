86f79ed45db7feeea9cb174721143ed8a63cd75c
MaheshRavi007 <maheshravi2712@gmail.com>
2021-05-20 13:50:48 +0530

    fixed code

--- script.js
+++ script.js
@@ -24,39 +24,29 @@ document.querySelector('.check'),addEventListener('click',function(){
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('number').style.width='30rem'
         
-        }else if(guess!==secretNumber){
-            if(score>1){
-                document.querySelector('.message').textContent=gess>secretNumber?'Too High Buddy!!':'Too Low Buddy!!';
-                score--;
-                document.querySelector('.score').textContent=score;
-                }else{
-                    document.querySelector('.message').textContent='Just Press Alt+F4 and Walkaway Buddy!!'
-                    document.querySelector('.score').textContent=0;
-                    document.querySelector('body').style.backgroundColor='#ff0000';
-            }
         }
-    //     // too high
-    // }else if(guess > secretNumber){
-    //     if(score >1){
-    //         document.querySelector('.message').textContent='Too High Buddy!!';
-    //         score--;
-    //         document.querySelector('.score').textContent=score;
-    //     }else{
-    //         document.querySelector('.message').textContent='Just Press Alt+F4 and Walkaway Buddy!!'
-    //         document.querySelector('.score').textContent=0;
-    //         document.querySelector('body').style.backgroundColor='#ff0000';
-    //     }
-    //     // too low
-    // }else if(guess < secretNumber){
-    //     if(score>1){
-    //     document.querySelector('.message').textContent='Too Low Buddy!!';
-        // score--;
-        // document.querySelector('.score').textContent=score;
-        // }else{
-        //     document.querySelector('.message').textContent='Just Press Alt+F4 and Walkaway Buddy!!'
-        //     document.querySelector('.score').textContent=0;
-        //     document.querySelector('body').style.backgroundColor='#ff0000';
-    //     }
+        // too high
+    }else if(guess > secretNumber){
+        if(score >1){
+            document.querySelector('.message').textContent='Too High Buddy!!';
+            score--;
+            document.querySelector('.score').textContent=score;
+        }else{
+            document.querySelector('.message').textContent='Just Press Alt+F4 and Walkaway Buddy!!'
+            document.querySelector('.score').textContent=0;
+            document.querySelector('body').style.backgroundColor='#ff0000';
+        }
+        // too low
+    }else if(guess < secretNumber){
+        if(score>1){
+        document.querySelector('.message').textContent='Too Low Buddy!!';
+        score--;
+        document.querySelector('.score').textContent=score;
+        }else{
+            document.querySelector('.message').textContent='Just Press Alt+F4 and Walkaway Buddy!!'
+            document.querySelector('.score').textContent=0;
+            document.querySelector('body').style.backgroundColor='#ff0000';
+        }
     }
 });
 document.querySelector('.again').addEventListener
